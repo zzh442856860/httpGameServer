d054777e83078269640dd3ba265094631d8375c4
zzh <zzh@zzh-VirtualBox.(none)>
2014-07-11 00:24:41 +0800

    mysql s

--- lua/test.lua
+++ lua/test.lua
@@ -26,3 +26,28 @@ if not ok then
 end
 
 ngx.say("set result: ", ok)
+
+local mysql = require "resty.mysql"
+local db, err = mysql:new()
+if not db then
+	ngx.say("failed to instantiate mysql: ", err)
+	return
+end
+
+db:set_timeout(1000)
+
+local ok, err, errno, sqlstate = db:connect{
+	host = "127.0.0.1",
+	port = 3306,
+	database = "ngx_test",
+	user = "ngx_test",
+	password = "ngx_test",
+	max_packet_size = 1024 * 1024
+}
+
+if not ok then
+	ngx.say(""failed to connect: ", err, ": ", errno, " ", sqlstate")
+	return
+end
+
+ngx.say("connected to mysql.")
